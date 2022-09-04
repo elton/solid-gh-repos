97e35869d990723835da5b9524ea662e9db72dbb
Elton Zheng <elton.zheng.sh@gmail.com>
2022-09-04 18:59:55 +0800

    initial the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# SolidJS Crash Course
+
+## References
+
+[Youtube tutorial](https://www.youtube.com/watch?v=WDodWU-B-aY&list=PLB4w8uonXmpk9qSJGiFYc_RGQfk_RxE5k)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return <div class='text-3xl text-teal-700'>Hello, world!</div>;
+};
+
+export default App;
